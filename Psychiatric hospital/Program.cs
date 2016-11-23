86f5bcb9d381d8800c6f2111a140a5598178ed56
pandizas <pandizas@abv.bg>
2016-11-23 16:16:05 +0200

    PSYCHOSIS

---
+++ Psychiatric hospital/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Psychiatric_hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WE ARE HERE IN THE DOTA 2 PSYCHIATRIC HOSPITAL");
+        }
+    }
+}
